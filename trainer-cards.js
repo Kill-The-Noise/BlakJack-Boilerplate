cc9c589d2650be079353a778837a2114d5dd27fe
BlakJack <BlakJack@users.noreply.github.com>
2014-07-14 21:50:25 +0530

    Create trainer-cards.js

---
+++ trainer-cards.js
@@ -0,0 +1,135 @@
+/**
+ * Trainer Cards
+ *
+ * This is where the trainer cards commands
+ * are located.
+ *
+ */
+
+var trainerCards = {
+	aananth: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-x.gif width="150" length="150"><img src=http://i.imgur.com/afSRAAO.png width="250"><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-y.gif img width="150" length="150"></center>');
+	},
+	
+        chaarizard: 'char',
+        charizard: 'char',
+        char: function(target,room,user) {
+        	if (!this.canBroadcast()) return;
+        	this.sendReply('|html|<div class="char"><center><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-x.gif height=125><img src=http://i.imgur.com/j1hggX6.png height=90><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-y.gif height=150></center><br><font size=2><center><font color=red><b>Quote:</b></font> Nobody is perfetc</center></font></div>');
+        },
+
+	arsh: 'blakjack',
+	arshmalik: 'blakjack',
+	blakjack: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://i.imgur.com/otDPUQU.png"><br><img src="http://i.imgur.com/Wdthjon.png"><img src="http://i.imgur.com/dck9vdP.png"><img src="http://i.imgur.com/5VqH7tF.png"><br><font color="brown"><blink>Ace: Swellow</blink><br><font color="brown">Haters Gonna Hate, Potatotes Gonna Potate But nCrypt\'s Gonna Masturbate');
+	},
+
+	surfersunite: 'surfing',
+	surfing: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://i.imgur.com/U2eSHfF.png"><br><img src="http://i.imgur.com/RNy1lDq.gif"><br><font color="black"><b>Ace:</font><blink><font color="purple"> Gallade </font></blink><br><font color="orange"><i>To defeat a psychic you must predict like a psychic.</i></b>');
+	},
+
+	zero: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('Zero Toxic <br />' +
+	        '<i>"Just Give Up You Cant Win"</i> <br />' +
+	        'Ace: Revenankh <br />' +
+	        '<img src="http://pokecharms.com/data/trainercardmaker/characters/custom/Other/Male-073.png"><img src="http://cap.smogon.com/Sprites/frontshiny-mrevenankh.png">');
+	},
+
+	boo118: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('boo118 <br />' +
+	        '<i>"Get Ready To Be Trolled"</i> <br />' +
+	        'Ace: Porygon 2 <br />' +
+	        '<img src="http://play.pokemonshowdown.com/sprites/bwani/porygon2.gif">');
+
+	},
+
+	arshm: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://i.imgur.com/otDPUQU.png"><br><img src="http://i.imgur.com/eZw2ZzA.png"><img src="http://i.imgur.com/V2pL6Xe.png"><img src="http://pldh.net/media/pokemon/conquest/sprite/398.png"><br><font color="brown"><blink>Ace: Staraptor</blink><br><font color="brown">Trolltraptor is gonna troll ur ass off');
+	},
+
+	enzarif: 'zarif',
+	zarif: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://i.imgur.com/JetHrTD.png"><center><img src="http://i.imgur.com/lC0aRUH.gif"><img src="http://i.imgur.com/FpaGoj7.gif"><img src="http://i.imgur.com/3EIY2d9.png"><right><br /><center><b>Ace: </b>Infernape</center><br /><center><b></b>Haters gonna hate,Bitches gonna Bitch,Trolls gonna Troll.But I (Zarif) will always be DerpMan</center>');
+	},
+
+	ucn: 'n',
+	n: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://www.smogon.com/media/forums/data/avatars/l/13/13073.jpg.m.1375935517"><br><img src="http://pldh.net/media/pokemon/conquest/sprite/392.png"><img src="http://i.imgur.com/KyLb7Xb.gif"><img src="http://i.imgur.com/Z7f9imD.png"><br><font color="orange"><blink> Ace: Infernape</font></blink><br><font color="brown"> Kickin\' Ass Since Day 1');
+	},
+
+	sooperpooper: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://i.imgur.com/ALqTE5D.png"><img src="http://i.imgur.com/Gc8Rirx.gif"><br><font color="black"><b> Ace:<BLINK></font><font color="purple"> Mega Ampharos </font></blink><br><i><font color="indigo"> Did I ever tell you the definition... of insanity?');
+	},
+
+	yousef: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://pldh.net/media/sugimori/091.png"><br><font color="blue"><font size="6"><b><i>FunnyTrainerYousef</b></i></font></font><br><font color="red"><blink>Ace: Cloyster</blink></font><br>Ice, Ice Baby');
+	},
+
+	mac: 'macrarazy',
+	e4mac: 'macrarazy',
+	professormac: 'macrarazy',
+	e6mac: 'macrarazy',
+	macrarazy: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://media-cerulean.cursecdn.com/attachments/thumbnails/5/622/530/530/mega_lucario.png" height="180" width="150"><br />' +
+	        '<img src="http://th03.deviantart.net/fs70/PRE/i/2014/003/8/1/mega_aggron_by_theangryaron-d70p759.png" height="130" width="150"> <img src="http://i.imgur.com/91GZs2L.gif"> <img src="http://fc06.deviantart.net/fs70/f/2013/285/4/7/mega_aggron_tramplin__the_lawn_by_brandon_stuart-d6q5051.png" height="130" width="160"><br />' +
+	        '<b><blink>Ace: Mega Aggron</blink><br />' +
+	        '<font color=gray>Sometimes... Steel is too much for you!</font></b>');
+	},
+
+	messi: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<img src="http://pldh.net/media/pokemon/ken_sugimori/update_bw/646-black.png"><img src="http://i.imgur.com/MWzPDNe.gif"><img src="http://pldh.net/media/sugimori/373.png"><br><center><font color="black">Ace:</font><blink><font color="lightblue"> Salamence and Kyurem-B</blink></font><br><i><font color="darkblue">Do you think Messi is awesome? Then im more awesome cus i have a 10 in front of it.');
+	},
+
+	avada: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://fc07.deviantart.net/fs70/f/2013/121/a/1/shadow_lugia_sprite_by_pokedan1-d63sjcf.png"><br><img src="http://fc03.deviantart.net/fs42/f/2009/158/2/3/Shadow_Lugia_Sprite_by_Agirl3003.png"><img src="http://s10.postimg.org/ih3nnrcll/cooltext1487828506.png"><img src="http://img-cache.cdn.gaiaonline.com/f987b8387b2c162d53317c35338e21e1/http://i103.photobucket.com/albums/m128/angletic_light/Shadow_Lugia_HGSS_Sprite_by_Quanyails.gif"><br><font color="blue"><blink>Ace: Shadow Lugia</blink><br><font color="blue">Expect The Unexpected.');
+	},
+
+	arjunb: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://i.imgur.com/VwCa2CS.png"> <img src="http://imgur.com/VxNGram.gif"> <img src="http://i.imgur.com/hpxiwWM.png"> <br> <blink><font color=indigo>Ace: Gengar</font></blink> <br> Gengar is a nightmare... and a nightmare is DEATH!');
+	},
+
+	ncrypt: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://i.imgur.com/rdSrtBA.png"><br />' +
+	        '<img src="http://i.imgur.com/VFeaIXd.gif"><img src="http://i.imgur.com/74K5o1L.gif"><img src="http://i.imgur.com/VFeaIXd.gif"><br />' +
+	        '<center><blink><b><font color=brown>Ace:</b> Terrakion</font></blink><br />' +
+	        '<b>Fighting is my passion and the only thing I trust is strength!');
+	},
+
+	drascension: 'ascension',
+	ascension: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src=http://i.imgur.com/F5AhtJ4.gif><img src=http://i.imgur.com/TWdRcTB.png><img src=http://i.imgur.com/fUqTeqh.gif><br />' +
+	        '<b><blink>Ace:</b> Hitmonlee and Machamp</blink><br />' +
+	        '<b>PUNCH! KICK! BAM! SLAM!');
+
+	},
+	anrindas: 'anrin',
+	anrin: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://pldh.net/media/pokemon/gen6/xy-animated/381.gif"><br><img src="http://pldh.net/media/pokemon/gen6/xy-animated/445.gif"><img src="http://s29.postimg.org/hof2hh0xz/cooltext1560999349.png"><img src="http://pldh.net/media/pokemon/gen6/xy-animated/646-black.gif"><br><font color="blue"><blink>Ace: Kyurem-Black</blink><br><font color="blue">Outrage is Rageing with power until death.');
+	},
+        darkshadow6: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://r42.cooltext.com/rendered/cooltext1562813497.png"> <img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/charizard-megax.gif"> <img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/venusaur-mega.gif"> <img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/blastoise-mega.gif"> <br> <font color = red> <blink> Ace: Charizard Mega X </blink> </font> <br> <font color=blue> Get Ready To Face The Flying Master </font> </center>');
+	},	
+
+};
+
+Object.merge(CommandParser.commands, trainerCards);
+exports.trainerCards = trainerCards;

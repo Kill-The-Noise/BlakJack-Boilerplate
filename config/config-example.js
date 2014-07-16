773036637d5402dbf1ebf6ba7dbb964aa680645f
BlakJack <BlakJack@users.noreply.github.com>
2014-07-16 22:09:56 +0530

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -1,10 +1,10 @@
 // The server port - the port to run Pokemon Showdown under
-exports.port = 8000;
+exports.port = 17000;
 
 // The server id - the id specified in the server registration.
 //   This should be set properly especially when there are more than one
 //   pokemon showdown server running from the same IP
-exports.serverId = 'ktnleague';
+exports.serverId = 'mega';
 
 // proxyIps - proxy IPs with trusted X-Forwarded-For headers
 //   This can be either false (meaning not to trust any proxies) or an array

2f2574eef52b75ed896cb9a99b56b4d90c03e191
BlakJack <BlakJack@users.noreply.github.com>
2014-07-16 22:13:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -467,7 +467,3 @@ global.Components = require('./components.js');
 global.Poll = require('./core.js').core.poll();
 
 global.SysopAccess = require('./core.js').sysopAccess();
-
-global.casino = require('./casino.js');
-
-global.trainerCards = require('./trainer-cards.js');

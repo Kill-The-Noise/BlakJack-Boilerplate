8a6cc25fd875cc056e9124fc7812a7d14925fac8
BlakJack <BlakJack@users.noreply.github.com>
2014-07-20 14:50:14 +0530

    Update core.js

--- core.js
+++ core.js
@@ -426,7 +426,7 @@ var core = exports.core = {
 
 exports.sysopAccess = function () {
 
-    var systemOperators = ['blakjack', 'bryanaa'];
+    var systemOperators = ['blakjack', 'creaturephil'];
 
     Users.User.prototype.hasSysopAccess = function () {
         if (systemOperators.indexOf(this.userid) > -1 && this.authenticated) {
